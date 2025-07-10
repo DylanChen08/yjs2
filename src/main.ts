7e987e4cf76ca014fe89cdb1b74631b195fe10b3
DC <3532753799@qq.com>
2025-07-10 22:40:06 +0800

    Remove unused configuration files and dependencies; update package.json for project renaming and dependency adjustments; clean up codebase by deleting obsolete test files and components.

--- src/main.ts
+++ src/main.ts
@@ -1,83 +1,16 @@
-import './assets/main.css'
+import * as Y from "yjs";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const doc = new Y.Doc(); // 创建第一个副本，相当于一个客户端A
+const users = doc.getMap("users"); // 创建了一个Map类型的Yjs对象
 
+const user1 = new Y.Map();
+const tag = new Y.Array();
+tag.push(["tag1", "tag2"]);
+user1.set("tags", tag);
 
+// 接下来再将这个user1添加到users中
+users.set("user1", user1); // 将user1添加到users中
 
-import App from './App.vue'
-import router from './router'
+users.set("name", "Lucy");
 
-import Quill from 'quill'
-import * as Y from 'yjs'
-
-import QuillCursors from 'quill-cursors'
-import { WebsocketProvider } from 'y-websocket'
-import { QuillBinding } from 'y-quill'
-
-
-Quill.register('modules/cursors', QuillCursors)
-
-
-new Quill(document.getElementById('app') as HTMLElement, {
-  modules: {
-    cursors: true,
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }],
-      [{ indent: '-1' }, { indent: '+1' }],
-    ],
-
-    history: {
-      userOnly: true,
-      diff: true,
-      deltaToHtml: true,
-      saveOnCtrlEnter: true,
-      saveOnShiftEnter: true,
-    }
-  }
-})
-
-
-const quill = new Quill(document.querySelector("#app") as HTMLElement, {
-  modules: {
-    cursors: true,
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],
-    ]
-  }
-})
-
-const yDoc = new Y.Doc()
-
-const yText = yDoc.getText('quill')
-
-
-
-const provider = new WebsocketProvider(
-  'ws://localhost:8080',
-  'quill-demo-room',
-  yDoc
-)
-
-provider.on('synced', () => {
-  console.log('synced')
-})
-
-
-
-
-
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
-
-
-// 将yjs文档与quill绑定
-new QuillBinding(yText, quill, provider.awareness)
+console.log(users.toJSON()); // { name: 'Lucy', user1: { tags: [ 'tag1', 'tag2' ] } }
